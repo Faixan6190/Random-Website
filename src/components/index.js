2a14814047ad46b13ce2edbd6771980162b6f6a7
Faixan6190 <faixan6190@gmail.com>
2024-06-02 20:30:19 +0500

    add files

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+export { default as Article } from "./article/Article";
+export { default as Brand } from "./brand/Brand";
+export { default as CTA } from "./cta/CTA";
+export { default as Feature } from "./feature/Feature";
+export { default as Navbar } from "./navbar/Navbar";
+
+// import Article from "./article/Article";
+// import Brand from "./brand/Brand";
+// import CTA from "./cta/CTA";
+// import Feature from "./feature/Feature";
+// import Navbar from "./navbar/Navbar";
+
+// export { Article, Brand, CTA, Feature, Navbar };
