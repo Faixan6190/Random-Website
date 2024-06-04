c534fcbc82d224800bebf0590bc50d77adf55b5a
Faixan6190 <faixan6190@gmail.com>
2024-06-04 20:37:49 +0500

    change

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,9 @@ const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo"></div>
+        <div className="gpt3__navbar-links_logo">
+          <p></p>
+        </div>
       </div>
     </div>
   );
